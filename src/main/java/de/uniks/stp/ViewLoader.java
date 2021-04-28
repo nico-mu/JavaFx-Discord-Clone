f935505f897470cac8a010536b4cd93efdf414bf
Marco Wennesz <marco.wennesz@student.uni-kassel.de>
2021-04-28 22:28:13 +0200

    [TASK] Implement concept for user entries

--- src/main/java/de/uniks/stp/ViewLoader.java
+++ src/main/java/de/uniks/stp/ViewLoader.java
@@ -1,5 +1,8 @@
-package de.uniks.stp.view;
+package de.uniks.stp;
 
+import de.uniks.stp.component.Components;
+import de.uniks.stp.view.Languages;
+import de.uniks.stp.view.Views;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
@@ -7,24 +10,31 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ViewLoader {
     public static Parent loadView(final Views alias) {
+        return getParent(alias.path);
+    }
+
+    public static Parent loadComponent(final Components alias) {
+        return getParent(alias.path);
+    }
+
+    private static Parent getParent(final URL path) {
         Parent load = null;
         try {
             //add classloader for bundle directory to load it into resource bundle
-            ClassLoader loader = new URLClassLoader(new URL[]{ViewLoader.class.getResource("../bundle/")});
+            ClassLoader loader = new URLClassLoader(new URL[]{ViewLoader.class.getResource("./bundle/")});
 
             //TODO: insert current used language here (accord.getLanguage?)
             //load resource bundle for given language
             ResourceBundle resourceBundle = ResourceBundle.getBundle("language", new Locale(Languages.GERMAN.key), loader);
 
             //load view with given resource bundle
-            load = FXMLLoader.load(Objects.requireNonNull(ViewLoader.class.getResource(alias.path)), resourceBundle);
+            load = FXMLLoader.load(path, resourceBundle);
         } catch (IOException e) {
-            System.err.println("View " + alias.path + " could not be loaded.");
+            System.err.println("View " + path.getPath() + " could not be loaded.");
             e.printStackTrace();
         }
         return load;
--- src/main/java/de/uniks/stp/view/Views.java
+++ src/main/java/de/uniks/stp/view/Views.java
@@ -1,11 +1,13 @@
 package de.uniks.stp.view;
 
+import java.net.URL;
+
 public enum Views {
     START_SCREEN("StartScreen.fxml"); // TODO: To be replaced
 
-    public final String path;
+    public final URL path;
 
     Views(String path) {
-        this.path = path;
+        this.path = Views.class.getResource(path);
     }
 }
