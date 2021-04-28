5c594ac4fc415486429ba9be0cebce2db731c00c
Niklas Hahn <niklas@hahn-online.org>
2021-04-28 21:22:17 +0200

    [CHORE] remove deprecated method from editor and add UserKeyProvider to StageManager

--- src/main/java/de/uniks/stp/Editor.java
+++ src/main/java/de/uniks/stp/Editor.java
@@ -9,12 +9,6 @@ public class Editor {
     // Connection to model root object
     private Accord accord;
 
-    //  Will be removed in TG21-19's UserKeyProvider
-    @Deprecated
-    public String getUserkey() {
-        return "";
-    }
-
     public Accord getOrCreateAccord() {
         if (Objects.isNull(accord)) {
             accord = new Accord().setLanguage(Languages.GERMAN.key);
