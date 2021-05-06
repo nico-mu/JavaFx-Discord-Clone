2876397789152d453b4564179d608a4f2fee1257
Armin Kozik <arkozik@student.uni-kassel.de>
2021-05-06 19:24:51 +0200

    [CHORE] add servername popup

--- src/main/java/de/uniks/stp/controller/TestScreenController.java
+++ src/main/java/de/uniks/stp/controller/TestScreenController.java
@@ -2,6 +2,7 @@ package de.uniks.stp.controller;
 
 import de.uniks.stp.Editor;
 import de.uniks.stp.network.RestClient;
+import javafx.scene.control.TextInputDialog;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 
@@ -9,6 +10,7 @@ public class TestScreenController implements ControllerInterface {
 
     private final Editor editor;
     private final RestClient restClient;
+    private String serverName;
 
     public TestScreenController(Editor editor) {
         this.editor = editor;
@@ -17,8 +19,25 @@ public class TestScreenController implements ControllerInterface {
 
     @Override
     public void init() {
-        System.out.println("testscreencntrl init called");
+        //openServerNamePopup();
         restClient.createServer("GUAVE_TEST_SERVER", editor.getOrCreateAccord().getUserKey(), this::handleCreateServerResponse);
+    }
+
+    //FIXME: doesn't work because fxml file is missing
+    private void openServerNamePopup() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Server name");
+        dialog.setHeaderText("Please the servername: ");
+        dialog.setContentText("Servername: ");
+
+        dialog.showAndWait().ifPresent((name) -> {
+            //do it like this?
+            if (name != null && !name.equals("")) {
+                restClient.createServer(name, editor.getOrCreateAccord().getUserKey(), this::handleCreateServerResponse);
+            } else {
+                return;
+            }
+        });
     }
 
     private void handleCreateServerResponse(HttpResponse<JsonNode> response) {
