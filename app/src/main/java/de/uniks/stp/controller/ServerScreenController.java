10d3274f91c566140d439be63428f2fee94b34e8
Niklas Hahn <niklas@hahn-online.org>
2021-05-13 22:30:32 +0200

    [FEATURE] Add new server view, which now displays server name

--- app/src/main/java/de/uniks/stp/controller/ServerScreenController.java
+++ app/src/main/java/de/uniks/stp/controller/ServerScreenController.java
@@ -2,6 +2,7 @@ package de.uniks.stp.controller;
 
 import de.uniks.stp.Constants;
 import de.uniks.stp.Editor;
+import de.uniks.stp.ViewLoader;
 import de.uniks.stp.annotation.Route;
 import de.uniks.stp.model.Server;
 import de.uniks.stp.router.RouteArgs;
@@ -9,43 +10,35 @@ import de.uniks.stp.router.RouteInfo;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
+import static de.uniks.stp.view.Views.SERVER_SCREEN;
+
 @Route(Constants.ROUTE_MAIN + Constants.ROUTE_SERVER)
 public class ServerScreenController implements ControllerInterface {
 
-    private Parent view;
-    private AnchorPane anchorPane;
+    private static final String SERVER_NAME_LABEL_ID = "#server-name-label";
+    private AnchorPane view;
+    private FlowPane serverScreenView;
     private final Editor editor;
     private final Server model;
+    private Label serverNameLabel;
 
     public ServerScreenController(Parent view, Editor editor, Server model) {
-        this.view = view;
+        this.view = (AnchorPane)view;
         this.editor = editor;
         this.model = model;
-        anchorPane = (AnchorPane) view;
     }
 
     @Override
     public void init() {
-        VBox vBox = new VBox();
-        anchorPane.getChildren().add(vBox);
-
-        Label label = new Label("Server Screen id: ");
-        label.setTextFill(new Color(1, 1, 1, 1));
-        label.setFont(new Font(50));
-        label.setPrefHeight(200);
-        label.setPrefWidth(400);
-        vBox.getChildren().add(label);
-
-        Label label2 = new Label( model.getId());
-        label2.setTextFill(new Color(1, 1, 1, 1));
-        label2.setFont(new Font(25));
-        label2.setPrefHeight(200);
-        label2.setPrefWidth(400);
-        vBox.getChildren().add(label2);
+       serverScreenView = (FlowPane) ViewLoader.loadView(SERVER_SCREEN);
+       view.getChildren().add(serverScreenView);
+       serverNameLabel = (Label)view.lookup(SERVER_NAME_LABEL_ID);
+       serverNameLabel.setText(model.getName());
     }
 
     @Override
