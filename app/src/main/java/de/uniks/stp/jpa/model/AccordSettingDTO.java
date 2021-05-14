e7fc6678c77a6438d1751cead34ca2c48d29830f
Marco Wennesz <marco.wennesz@student.uni-kassel.de>
2021-05-14 16:06:11 +0200

    [TASK] Add JPA mapping for AccordSettingDTO

---
+++ app/src/main/java/de/uniks/stp/jpa/model/AccordSettingDTO.java
@@ -0,0 +1,35 @@
+package de.uniks.stp.jpa.model;
+
+import de.uniks.stp.jpa.AccordSettingKey;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "accord_setting")
+public class AccordSettingDTO {
+    @Id
+    @Column(name = "setting_key")
+    @Enumerated(EnumType.STRING)
+    private AccordSettingKey key;
+
+    @Column(name = "setting_value")
+    private String value;
+
+    public AccordSettingKey getKey() {
+        return key;
+    }
+
+    public AccordSettingDTO setKey(AccordSettingKey key) {
+        this.key = key;
+        return this;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public AccordSettingDTO setValue(String value) {
+        this.value = value;
+        return this;
+    }
+}
