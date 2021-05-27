3746036959aa47c8a5cee4d7af23032320847bfa
DanielErhard <57105258+DanielErhard@users.noreply.github.com>
2021-05-27 03:51:38 +0200

    [TEST] add first test for create category

--- app/src/test/java/de/uniks/stp/controller/ServerSettingsTest.java
+++ app/src/test/java/de/uniks/stp/controller/ServerSettingsTest.java
@@ -5,6 +5,8 @@ import de.uniks.stp.Constants;
 import de.uniks.stp.Editor;
 import de.uniks.stp.StageManager;
 import de.uniks.stp.ViewLoader;
+import de.uniks.stp.model.Category;
+import de.uniks.stp.model.Channel;
 import de.uniks.stp.model.Server;
 import de.uniks.stp.model.User;
 import de.uniks.stp.network.NetworkClientInjector;
@@ -14,10 +16,12 @@ import de.uniks.stp.router.RouteArgs;
 import de.uniks.stp.router.Router;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import kong.unirest.Callback;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
+import kong.unirest.json.JSONArray;
 import kong.unirest.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -110,5 +114,59 @@ public class ServerSettingsTest {
             Assertions.assertEquals(newName, serverLabel.getText());
         });
         Assertions.assertEquals(newName, editor.getServer(serverId).getName());
+    }
+
+    @Test
+    public void testCreateCategory(FxRobot robot){
+        // prepare start situation
+        Editor editor = StageManager.getEditor();
+
+        editor.getOrCreateAccord().setCurrentUser(new User().setName("Test")).setUserKey("123-45");
+
+        String serverName ="Plattis Server";
+        String serverId ="12345678";
+        Server server = new Server().setName(serverName).setId(serverId);
+        editor.getOrCreateAccord().getCurrentUser().withAvailableServers(server);
+
+        RouteArgs args = new RouteArgs().addArgument(":id", serverId);
+        Platform.runLater(() -> Router.route(Constants.ROUTE_MAIN + Constants.ROUTE_SERVER, args));
+        WaitForAsyncUtils.waitForFxEvents();
+
+        // assert correct start situation
+        Assertions.assertEquals(1, editor.getOrCreateAccord().getCurrentUser().getAvailableServers().size());
+        Assertions.assertEquals(0, editor.getServer(serverId).getCategories().size());
+        Platform.runLater(() -> {
+            VBox serverChannelOverview = robot.lookup("#server-channel-overview").query();
+            //serverChannelOverview.getChildren().get(0)
+        });
+
+        // prepare creating category
+        robot.clickOn("#settings-label");
+        robot.clickOn("#create-menu-item");
+
+        // insert name
+        String categoryName = "useful category";
+        robot.write(categoryName);
+        robot.clickOn("#create-button");
+
+        JSONObject data = new JSONObject().put("id", "999")
+            .put("name", categoryName)
+            .put("server", serverId)
+            .put("channels", new JSONArray());
+        JSONObject j = new JSONObject().put("status", "success").put("message", "").put("data", data);
+        when(res.getBody()).thenReturn(new JsonNode(j.toString()));
+        when(res.isSuccess()).thenReturn(true);
+
+        verify(restMock).createCategory(eq(serverId), eq(categoryName), callbackCaptor.capture());
+        Callback<JsonNode> callback = callbackCaptor.getValue();
+        callback.completed(res);
+
+        // check for correct reactions
+        Assertions.assertEquals(1, editor.getServer(serverId).getCategories().size());
+        Assertions.assertEquals(categoryName, editor.getServer(serverId).getCategories().get(0).getName());
+        Platform.runLater(() -> {
+            VBox serverChannelOverview = robot.lookup("#server-channel-overview").query();
+            //serverChannelOverview.getChildren().get(0)
+        });
     }
 }
