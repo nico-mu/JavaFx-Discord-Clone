4e13eda912f699cef018cb134fe8b4a4fc5a21c5
Luca Hertel <lucah199800@gmail.com>
2021-05-28 17:33:20 +0200

    add styling

--- app/src/main/java/de/uniks/stp/modal/AddChannelModal.java
+++ app/src/main/java/de/uniks/stp/modal/AddChannelModal.java
@@ -4,13 +4,10 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXTextField;
 import de.uniks.stp.Constants;
-import de.uniks.stp.Editor;
 import de.uniks.stp.ViewLoader;
 import de.uniks.stp.component.UserCheckList;
 import de.uniks.stp.component.UserCheckListEntry;
-import de.uniks.stp.controller.LoginScreenController;
 import de.uniks.stp.model.Category;
-import de.uniks.stp.model.Channel;
 import de.uniks.stp.model.User;
 import de.uniks.stp.network.NetworkClientInjector;
 import de.uniks.stp.network.RestClient;
@@ -18,26 +15,22 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
-import kong.unirest.json.JSONArray;
-import kong.unirest.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
-public class AddChannelModal extends AbstractModal{
+public class AddChannelModal extends AbstractModal {
     private static final Logger log = LoggerFactory.getLogger(AddChannelModal.class);
 
     public static final String ADD_CHANNEL_NAME_TEXTFIELD = "#add-channel-name-textfield";
     public static final String PRIVILEGED_CHECKBOX = "#privileged-checkbox";
     public static final String FILTER_USER_TEXTFIELD = "#filter-user-textfield";
-    public static final String ADD_CHANNEL_USER_LIST_SCROLLPANE = "#add-channel-user-list-scrollpane";
     public static final String USER_CHECK_LIST_CONTAINER = "#user-check-list-container";
     public static final String ADD_CHANNEL_CREATE_BUTTON = "#add-channel-create-button";
     public static final String ADD_CHANNEL_CANCEL_BUTTON = "#add-channel-cancel-button";
@@ -45,33 +38,32 @@ public class AddChannelModal extends AbstractModal{
     private JFXTextField channelName;
     private JFXCheckBox privileged;
     private JFXTextField filter;
-    private Pane pane;
+    private HBox userCheckListContainer;
     private UserCheckList selectUserList;
     private JFXButton createButton;
     private JFXButton cancelButton;
     private Label errorLabel;
     private Category category;
-    private Editor editor;
     private HashMap<String, UserCheckListEntry> serverUserElementsMap;
     private RestClient restClient;
 
-    public AddChannelModal(Parent root, Category category, Editor editor){
+    public AddChannelModal(Parent root, Category category) {
         super(root);
         this.category = category;
-        this.editor = editor;
         this.restClient = NetworkClientInjector.getRestClient();
 
         setTitle(ViewLoader.loadLabel(Constants.LBL_CREATE_CHANNEL));
         channelName = (JFXTextField) view.lookup(ADD_CHANNEL_NAME_TEXTFIELD);
         privileged = (JFXCheckBox) view.lookup(PRIVILEGED_CHECKBOX);
         filter = (JFXTextField) view.lookup(FILTER_USER_TEXTFIELD);
-        pane = (Pane) view.lookup(USER_CHECK_LIST_CONTAINER);
+        userCheckListContainer = (HBox) view.lookup(USER_CHECK_LIST_CONTAINER);
         createButton = (JFXButton) view.lookup(ADD_CHANNEL_CREATE_BUTTON);
         cancelButton = (JFXButton) view.lookup(ADD_CHANNEL_CANCEL_BUTTON);
         errorLabel = (Label) view.lookup(ADD_CHANNEL_ERROR_LABEL);
 
         selectUserList = new UserCheckList();
-        pane.getChildren().add(selectUserList);
+        selectUserList.setMaxHeight(userCheckListContainer.getMaxHeight());
+        userCheckListContainer.getChildren().add(selectUserList);
 
         createButton.setOnAction(this::onCreatButtonClicked);
         cancelButton.setOnAction(this::onCancelButtonClicked);
@@ -82,10 +74,7 @@ public class AddChannelModal extends AbstractModal{
 
         serverUserElementsMap = new HashMap<>();
 
-        for(User user : category.getServer().getUsers()){
-            /*if(user.getName().equals(editor.getOrCreateAccord().getCurrentUser().getName())){
-                continue;
-            }*/
+        for (User user : category.getServer().getUsers()) {
             UserCheckListEntry userCheckListEntry = new UserCheckListEntry(user);
             serverUserElementsMap.put(user.getName(), userCheckListEntry);
             selectUserList.addUserCheckListEntry(userCheckListEntry);
@@ -94,8 +83,8 @@ public class AddChannelModal extends AbstractModal{
 
     private void filterUsers(String newValue) {
         selectUserList.clearUserCheckList();
-        for(String name : serverUserElementsMap.keySet()){
-            if(name.contains(newValue)){
+        for (String name : serverUserElementsMap.keySet()) {
+            if (name.contains(newValue)) {
                 selectUserList.addUserCheckListEntry(serverUserElementsMap.get(name));
             }
         }
@@ -109,13 +98,12 @@ public class AddChannelModal extends AbstractModal{
         String categoryId = category.getId();
 
         ArrayList<String> selectedUserNames = new ArrayList<>();
-        //selectedUserNames.add(editor.getOrCreateAccord().getCurrentUser().getName());
-        for(UserCheckListEntry userCheckListEntry : serverUserElementsMap.values()){
-            if(userCheckListEntry.isUserSelected()){
+        for (UserCheckListEntry userCheckListEntry : serverUserElementsMap.values()) {
+            if (userCheckListEntry.isUserSelected()) {
                 selectedUserNames.add(userCheckListEntry.getUserName());
             }
         }
-        restClient.createTextChannel(serverId,categoryId,chName,priv, selectedUserNames, this::handleCreateChannelResponse);
+        restClient.createTextChannel(serverId, categoryId, chName, priv, selectedUserNames, this::handleCreateChannelResponse);
     }
 
     private void setErrorMessage(String label) {
@@ -134,16 +122,16 @@ public class AddChannelModal extends AbstractModal{
     private void handleCreateChannelResponse(HttpResponse<JsonNode> jsonNodeHttpResponse) {
         log.debug("Received create server response: " + jsonNodeHttpResponse.getBody().toPrettyString());
 
-        if(jsonNodeHttpResponse.isSuccess()){
+        if (jsonNodeHttpResponse.isSuccess()) {
             Platform.runLater(this::close);
-        }else{
+        } else {
             log.error("Create server failed: " + jsonNodeHttpResponse.getBody().toPrettyString());
             String errorMessage = jsonNodeHttpResponse.getBody().getObject().getString("message");
-            if(errorMessage.equals("Missing name")){
+            if (errorMessage.equals("Missing name")) {
                 setErrorMessage(Constants.LBL_MISSING_NAME);
-            }else if (errorMessage.equals("Missing members")){
+            } else if (errorMessage.equals("Missing members")) {
                 setErrorMessage(Constants.LBL_MISSING_MEMBERS);
-            }else{
+            } else {
                 log.error("Unhandled create server response: " + jsonNodeHttpResponse.getBody().toPrettyString());
             }
         }
@@ -154,7 +142,7 @@ public class AddChannelModal extends AbstractModal{
     }
 
     @Override
-    public void close(){
+    public void close() {
         createButton.setOnAction(null);
         cancelButton.setOnAction(null);
         super.close();
