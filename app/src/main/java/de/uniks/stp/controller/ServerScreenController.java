bcb997ac28ab9a4a40afd7b4527cc872b1c3c723
Luca Hertel <lucah199800@gmail.com>
2021-06-05 17:53:08 +0200

    add styling to contextMenu

--- app/src/main/java/de/uniks/stp/controller/ServerScreenController.java
+++ app/src/main/java/de/uniks/stp/controller/ServerScreenController.java
@@ -83,6 +83,11 @@ public class ServerScreenController implements ControllerInterface {
         items.get(1).setOnAction(this::onEditServerClicked);
         items.get(2).setOnAction(this::onCreateCategoryClicked);
 
+        settingsContextMenu.setStyle("-fx-background-color: #2c2f33");
+        for(MenuItem menuItem : settingsContextMenu.getItems()) {
+            menuItem.setStyle("-fx-text-fill: White");
+        }
+
         settingsGearLabel.setOnMouseClicked(e -> settingsContextMenu.show(settingsGearLabel, Side.BOTTOM, 0, 0));
 
         categoryListController = new ServerCategoryListController(serverChannelOverview, editor, model);
