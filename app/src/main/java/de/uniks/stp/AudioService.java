d91d777a8cff0491d9b5039f4a8d6e1225947f6f
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2021-06-10 17:15:17 +0200

    [FIX] make audioservice non static

--- app/src/main/java/de/uniks/stp/AudioService.java
+++ app/src/main/java/de/uniks/stp/AudioService.java
@@ -1,10 +1,13 @@
 package de.uniks.stp;
 
+import de.uniks.stp.model.Accord;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -13,28 +16,38 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class AudioService {
-    private static Media notificationSoundFile;
-    private static String notificationSoundFileName;
-    private static final String headlessCheck = System.getProperty("testfx.headless");
+    private Media notificationSoundFile;
+    private String notificationSoundFileName;
+    private final String headlessCheck = System.getProperty("testfx.headless");
+    private final PropertyChangeListener notificationSoundPropertyChangeListener = this::onNotificationSoundPropertyChange;
+    private final Editor editor;
 
-    public static void setNotificationSoundFile(String name) {
+
+    public AudioService(Editor editor) {
+        setNotificationSoundFile("gaming-lock.wav");
+        this.editor = editor;
+        this.editor.getOrCreateAccord().listeners().addPropertyChangeListener(Accord.PROPERTY_NOTIFICATION_SOUND, notificationSoundPropertyChangeListener);
+    }
+
+    public void stop() {
+        this.editor.getOrCreateAccord().listeners().removePropertyChangeListener(Accord.PROPERTY_NOTIFICATION_SOUND, notificationSoundPropertyChangeListener);
+    }
+
+    public void setNotificationSoundFile(String name) {
         File file = Objects.requireNonNull(getNotificationSoundResource(name));
         notificationSoundFileName = file.getName();
         notificationSoundFile = new Media(file.toURI().toString());
     }
 
-    public static void playNotificationSound() {
+    public void playNotificationSound() {
         if (Objects.nonNull(headlessCheck) && headlessCheck.equals("true")) {
             return;
-        }
-        if (Objects.isNull(notificationSoundFile)) {
-            setNotificationSoundFile("gaming-lock.wav");
         }
         MediaPlayer mediaPlayer = new MediaPlayer(notificationSoundFile);
         mediaPlayer.play();
     }
 
-    public static File getNotificationSoundResource(String name) {
+    public File getNotificationSoundResource(String name) {
         URL resPath;
         if (name.equals(".")) {
             resPath = AudioService.class.getResource("audio/notification");
@@ -47,11 +60,15 @@ public class AudioService {
         return new File(resPath.getPath());
     }
 
-    public static File[] getNotificationSoundFiles() {
+    public File[] getNotificationSoundFiles() {
         return Objects.requireNonNull(getNotificationSoundResource(".")).listFiles();
     }
 
-    public static String getNotificationSoundFileName() {
+    public String getNotificationSoundFileName() {
         return notificationSoundFileName;
+    }
+
+    private void onNotificationSoundPropertyChange(PropertyChangeEvent propertyChangeEvent) {
+
     }
 }
