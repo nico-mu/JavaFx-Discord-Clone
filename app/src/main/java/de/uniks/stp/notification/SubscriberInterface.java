82a886092ab7a779bbe671153b3c77a961194e1f
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2021-06-15 12:07:57 +0200

    [CHORE] add java doc to services

--- app/src/main/java/de/uniks/stp/notification/SubscriberInterface.java
+++ app/src/main/java/de/uniks/stp/notification/SubscriberInterface.java
@@ -1,7 +1,17 @@
 package de.uniks.stp.notification;
 
 public interface SubscriberInterface {
+    /**
+     * Method triggered by the NotificationEvent in the onChannelMessage method.
+     * The event contains the channel which got a notification as source and the notification count of the channel.
+     * @param event NotificationEvent with source type Channel.
+     */
     void onChannelNotificationEvent(NotificationEvent event);
 
+    /**
+     * Method triggered by the NotificationEvent in the onUserMessage method.
+     * The event contains as source the user which got a notification and the notification count of the user.
+     * @param event NotificationEvent with source type User.
+     */
     void onUserNotificationEvent(NotificationEvent event);
 }
