752afe598593bd990c156b24973f9ddc729c13e4
Sebastian Koch <koch.sebastian@protonmail.com>
2021-06-18 03:51:46 +0200

    [CHORE] rewrite of MiniGameControllerTest

--- app/src/test/java/de/uniks/stp/controller/MiniGameControllerTest.java
+++ app/src/test/java/de/uniks/stp/controller/MiniGameControllerTest.java
@@ -3,6 +3,8 @@ package de.uniks.stp.controller;
 import de.uniks.stp.Constants;
 import de.uniks.stp.Editor;
 import de.uniks.stp.StageManager;
+import de.uniks.stp.ViewLoader;
+import de.uniks.stp.emote.EmoteTextArea;
 import de.uniks.stp.jpa.DatabaseService;
 import de.uniks.stp.jpa.model.DirectMessageDTO;
 import de.uniks.stp.model.User;
@@ -10,9 +12,14 @@ import de.uniks.stp.network.NetworkClientInjector;
 import de.uniks.stp.network.RestClient;
 import de.uniks.stp.network.WSCallback;
 import de.uniks.stp.network.WebSocketClient;
+import de.uniks.stp.router.RouteArgs;
 import de.uniks.stp.router.Router;
 import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.fxmisc.flowless.VirtualizedScrollPane;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,13 +30,16 @@ import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testfx.api.FxRobot;
+import org.testfx.api.FxRobotException;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
+import org.testfx.service.query.EmptyNodeQueryException;
 import org.testfx.service.query.NodeQuery;
 import org.testfx.util.WaitForAsyncUtils;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -73,7 +83,7 @@ public class MiniGameControllerTest {
     }
 
     @Test
-    public void testMiniGame(FxRobot robot) {
+    public void testMiniGameStart(FxRobot robot) {
         Editor editor = StageManager.getEditor();
 
         User currentUser = new User().setName(generateRandomString()).setId(generateRandomString());
@@ -84,7 +94,9 @@ public class MiniGameControllerTest {
             .setUserKey(generateRandomString())
             .withOtherUsers(otherUser);
 
-        Platform.runLater(() -> Router.route(Constants.ROUTE_MAIN + Constants.ROUTE_HOME));
+        Platform.runLater(() -> Router.route(
+            Constants.ROUTE_MAIN + Constants.ROUTE_HOME + Constants.ROUTE_PRIVATE_CHAT,
+            new RouteArgs().addArgument(Constants.ROUTE_PRIVATE_CHAT_ARGS, otherUser.getId())));
         WaitForAsyncUtils.waitForFxEvents();
 
         verify(webSocketMock, times(2)).inject(stringArgumentCaptor.capture(), wsCallbackArgumentCaptor.capture());
@@ -95,29 +107,79 @@ public class MiniGameControllerTest {
         }
         WSCallback currentUserCallback = endpointCallbackHashmap.get(Constants.WS_USER_PATH + currentUser.getName());
 
+        // Other user initializes the game
         JsonObject message = Json.createObjectBuilder()
             .add("channel", "private")
-            .add("timestamp", 1)
-            .add("message", "!play :handshake:")
+            .add("timestamp", new Date().getTime())
+            .add("message", MiniGameController.GameCommand.PLAY.command)
             .add("from", otherUser.getName())
             .add("to", currentUser.getName())
             .build();
         currentUserCallback.handleMessage(message);
-        message = Json.createObjectBuilder()
-            .add("channel", "!play :handshake:")
-            .add("timestamp", 2)
-            .add("message", "Test")
-            .add("from", otherUser.getName())
-            .add("to", currentUser.getName())
-            .build();
-        currentUserCallback.handleMessage(message);
+        WaitForAsyncUtils.waitForFxEvents();
 
+        VBox chatViewMessageInput = robot.lookup("#chatViewMessageInput").query();
+        EmoteTextArea messageInput = (EmoteTextArea) (((VirtualizedScrollPane<?>) chatViewMessageInput.getChildren().get(0))).getContent();
+
+        Platform.runLater(() -> {
+            messageInput.clear();
+            messageInput.appendText(MiniGameController.GameCommand.PLAY.command);
+        });
+        robot.clickOn("#chatViewSubmitButton");
         WaitForAsyncUtils.waitForFxEvents();
 
         // MiniGame should open
-        NodeQuery actionLabel = robot.lookup("#action-label");
-        Assertions.assertNotNull(actionLabel);
+        Node query = robot.lookup("#paper-button").query();
+        Assertions.assertNotNull(query);
 
+        // TODO: Check for correct outgoing websocket messages
+        robot.clickOn("#paper-button");
+        message = Json.createObjectBuilder()
+            .add("channel", "private")
+            .add("timestamp", new Date().getTime())
+            .add("message", MiniGameController.GameCommand.CHOOSE_SCISSOR.command)
+            .add("from", otherUser.getName())
+            .add("to", currentUser.getName())
+            .build();
+        currentUserCallback.handleMessage(message);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        Label actionLabel = robot.lookup("#action-label").query();
+
+        Assertions.assertEquals(ViewLoader.loadLabel(Constants.LBL_RESULT_LOSS), actionLabel.getText());
+
+        robot.clickOn("#revanche-button");
+
+        message = Json.createObjectBuilder()
+            .add("channel", "private")
+            .add("timestamp", new Date().getTime())
+            .add("message", MiniGameController.GameCommand.REVANCHE.command)
+            .add("from", otherUser.getName())
+            .add("to", currentUser.getName())
+            .build();
+        currentUserCallback.handleMessage(message);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        Assertions.assertEquals(ViewLoader.loadLabel(Constants.LBL_CHOOSE_ACTION), actionLabel.getText());
+
+        robot.clickOn("#rock-button");
+        message = Json.createObjectBuilder()
+            .add("channel", "private")
+            .add("timestamp", new Date().getTime())
+            .add("message", MiniGameController.GameCommand.CHOOSE_SCISSOR.command)
+            .add("from", otherUser.getName())
+            .add("to", currentUser.getName())
+            .build();
+        currentUserCallback.handleMessage(message);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        Assertions.assertEquals(ViewLoader.loadLabel(Constants.LBL_RESULT_WIN), actionLabel.getText());
+
+        // Close mini game
+        robot.clickOn("#cancel-button");
+        Assertions.assertThrows(FxRobotException.class, () -> {
+            robot.clickOn("#cancel-button");
+        });
     }
 
     @AfterEach
