b155c92186d2d40cf62835e4113ce8be21670cff
Sebastian Koch <koch.sebastian@protonmail.com>
2021-06-18 02:51:32 +0200

    [CHORE] add test setup for mini game controller

---
+++ app/src/test/java/de/uniks/stp/controller/MiniGameControllerTest.java
@@ -0,0 +1,134 @@
+package de.uniks.stp.controller;
+
+import de.uniks.stp.Constants;
+import de.uniks.stp.Editor;
+import de.uniks.stp.StageManager;
+import de.uniks.stp.jpa.DatabaseService;
+import de.uniks.stp.jpa.model.DirectMessageDTO;
+import de.uniks.stp.model.User;
+import de.uniks.stp.network.NetworkClientInjector;
+import de.uniks.stp.network.RestClient;
+import de.uniks.stp.network.WSCallback;
+import de.uniks.stp.network.WebSocketClient;
+import de.uniks.stp.router.Router;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.service.query.NodeQuery;
+import org.testfx.util.WaitForAsyncUtils;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(ApplicationExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+public class MiniGameControllerTest {
+    @Mock
+    private RestClient restMock;
+
+    @Mock
+    private WebSocketClient webSocketMock;
+
+    @Captor
+    private ArgumentCaptor<String> stringArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<WSCallback> wsCallbackArgumentCaptor;
+
+    private HashMap<String, WSCallback> endpointCallbackHashmap;
+    private StageManager app;
+
+    @Start
+    public void start(Stage stage) {
+        // start application
+        MockitoAnnotations.initMocks(this);
+        endpointCallbackHashmap = new HashMap<>();
+        NetworkClientInjector.setRestClient(restMock);
+        NetworkClientInjector.setWebSocketClient(webSocketMock);
+        StageManager.setBackupMode(false);
+        app = new StageManager();
+        app.start(stage);
+        DatabaseService.clearAllConversations();
+    }
+
+    private String generateRandomString() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Test
+    public void testMiniGame(FxRobot robot) {
+        Editor editor = StageManager.getEditor();
+
+        User currentUser = new User().setName(generateRandomString()).setId(generateRandomString());
+        User otherUser = new User().setName(generateRandomString()).setId(generateRandomString());
+
+        editor.getOrCreateAccord()
+            .setCurrentUser(currentUser)
+            .setUserKey(generateRandomString())
+            .withOtherUsers(otherUser);
+
+        Platform.runLater(() -> Router.route(Constants.ROUTE_MAIN + Constants.ROUTE_HOME));
+        WaitForAsyncUtils.waitForFxEvents();
+
+        verify(webSocketMock, times(2)).inject(stringArgumentCaptor.capture(), wsCallbackArgumentCaptor.capture());
+        List<WSCallback> wsCallbacks = wsCallbackArgumentCaptor.getAllValues();
+        List<String> endpoints = stringArgumentCaptor.getAllValues();
+        for (int i = 0; i < endpoints.size(); i++) {
+            endpointCallbackHashmap.putIfAbsent(endpoints.get(i), wsCallbacks.get(i));
+        }
+        WSCallback currentUserCallback = endpointCallbackHashmap.get(Constants.WS_USER_PATH + currentUser.getName());
+
+        JsonObject message = Json.createObjectBuilder()
+            .add("channel", "private")
+            .add("timestamp", 1)
+            .add("message", "!play :handshake:")
+            .add("from", otherUser.getName())
+            .add("to", currentUser.getName())
+            .build();
+        currentUserCallback.handleMessage(message);
+        message = Json.createObjectBuilder()
+            .add("channel", "!play :handshake:")
+            .add("timestamp", 2)
+            .add("message", "Test")
+            .add("from", otherUser.getName())
+            .add("to", currentUser.getName())
+            .build();
+        currentUserCallback.handleMessage(message);
+
+        WaitForAsyncUtils.waitForFxEvents();
+
+        // MiniGame should open
+        NodeQuery actionLabel = robot.lookup("#action-label");
+        Assertions.assertNotNull(actionLabel);
+
+    }
+
+    @AfterEach
+    void tear(){
+        restMock = null;
+        webSocketMock = null;
+        stringArgumentCaptor = null;
+        wsCallbackArgumentCaptor = null;
+        endpointCallbackHashmap = null;
+        Platform.runLater(app::stop);
+        WaitForAsyncUtils.waitForFxEvents();
+        app = null;
+    }
+}
