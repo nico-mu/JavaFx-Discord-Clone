7a7f1e91033168d0873bbaa76c7255b5ef21d53c
Luca Hertel <lucah199800@gmail.com>
2021-07-10 13:51:59 +0200

    [FIX] dont route on current user

--- app/src/main/java/de/uniks/stp/component/ServerUserListEntry.java
+++ app/src/main/java/de/uniks/stp/component/ServerUserListEntry.java
@@ -3,11 +3,16 @@ package de.uniks.stp.component;
 import dagger.assisted.Assisted;
 import dagger.assisted.AssistedFactory;
 import dagger.assisted.AssistedInject;
+import de.uniks.stp.Constants;
+import de.uniks.stp.Editor;
 import de.uniks.stp.ViewLoader;
 import de.uniks.stp.model.User;
+import de.uniks.stp.router.RouteArgs;
+import de.uniks.stp.router.Router;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 
 import java.io.IOException;
@@ -17,9 +22,13 @@ public class ServerUserListEntry extends HBox {
     private Label userNameLabel;
 
     private User user;
+    private final Router router;
+    Editor editor;
 
     @AssistedInject
     public ServerUserListEntry(ViewLoader viewLoader,
+                               Router router,
+                               Editor editor,
                                @Assisted final User user) {
         final FXMLLoader fxmlLoader = viewLoader.getFXMLComponentLoader(Components.USER_LIST_ENTRY);
         fxmlLoader.setRoot(this);
@@ -32,8 +41,11 @@ public class ServerUserListEntry extends HBox {
         }
 
         this.user = user;
+        this.router = router;
+        this.editor = editor;
 
         setUserName(user.getName());
+        this.setOnMouseClicked(this::handleClick);
     }
 
     public void setUserName(final String userName) {
@@ -43,5 +55,13 @@ public class ServerUserListEntry extends HBox {
     @AssistedFactory
     public interface ServerUserListEntryFactory {
         ServerUserListEntry create(User user);
+    }
+
+    private void handleClick(MouseEvent mouseEvent) {
+        if (user.getId().equals(editor.getOrCreateAccord().getCurrentUser().getId())) {
+            return;
+        }
+        RouteArgs args = new RouteArgs().addArgument(Constants.ROUTE_PRIVATE_CHAT_ARGS, user.getId());
+        router.route(Constants.ROUTE_MAIN + Constants.ROUTE_HOME + Constants.ROUTE_PRIVATE_CHAT, args);
     }
 }
