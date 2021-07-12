13e5ecce101cc76a6df380acd9e100b5e15abbfb
Luca Hertel <lucah199800@gmail.com>
2021-07-12 14:17:17 +0200

    [FEATURE] route to offline non chat partner

--- app/src/main/java/de/uniks/stp/component/ServerUserListEntry.java
+++ app/src/main/java/de/uniks/stp/component/ServerUserListEntry.java
@@ -64,6 +64,7 @@ public class ServerUserListEntry extends HBox {
         if (user.getId().equals(currentUser.getId())) {
             return;
         }
+        editor.getOrCreateChatPartnerOfCurrentUser(user.getId(), user.getName());
         RouteArgs args = new RouteArgs().addArgument(Constants.ROUTE_PRIVATE_CHAT_ARGS, user.getId());
         router.route(Constants.ROUTE_MAIN + Constants.ROUTE_HOME + Constants.ROUTE_PRIVATE_CHAT, args);
     }
