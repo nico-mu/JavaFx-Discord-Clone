2c1bb26129af30455bbacbd5a5e82d28842a4412
Marco Wennesz <marco.wennesz@student.uni-kassel.de>
2021-07-17 15:27:27 +0200

    [CHORE] Refactor ServerUserListEntry to extend UserListEntry

--- app/src/main/java/de/uniks/stp/component/ServerUserListEntry.java
+++ app/src/main/java/de/uniks/stp/component/ServerUserListEntry.java
@@ -6,52 +6,26 @@ import dagger.assisted.AssistedInject;
 import de.uniks.stp.Constants;
 import de.uniks.stp.Editor;
 import de.uniks.stp.ViewLoader;
-import de.uniks.stp.jpa.SessionDatabaseService;
 import de.uniks.stp.model.User;
 import de.uniks.stp.router.RouteArgs;
 import de.uniks.stp.router.Router;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 
-import java.io.IOException;
-
-public class ServerUserListEntry extends HBox {
-    @FXML
-    private Label userNameLabel;
-
-    private User user;
-    private final Router router;
+public class ServerUserListEntry extends UserListEntry {
     private final Editor editor;
+    private final Router router;
 
     @AssistedInject
     public ServerUserListEntry(ViewLoader viewLoader,
                                Router router,
                                Editor editor,
                                @Assisted final User user) {
-        final FXMLLoader fxmlLoader = viewLoader.getFXMLComponentLoader(Components.USER_LIST_ENTRY);
-        fxmlLoader.setRoot(this);
-        fxmlLoader.setController(this);
+        super(viewLoader, user);
         this.setId(user.getId() + "-ServerUserListEntry");
 
-        try {
-            fxmlLoader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
-
-        this.user = user;
-        this.router = router;
         this.editor = editor;
-
-        setUserName(user.getName());
-        this.setOnMouseClicked(this::handleClick);
-    }
-
-    public void setUserName(final String userName) {
-        userNameLabel.setText(userName);
+        this.router = router;
+        setOnMouseClicked(this::handleClick);
     }
 
     @AssistedFactory
--- app/src/main/java/de/uniks/stp/component/UserListEntry.java
+++ app/src/main/java/de/uniks/stp/component/UserListEntry.java
@@ -3,15 +3,11 @@ package de.uniks.stp.component;
 import dagger.assisted.Assisted;
 import dagger.assisted.AssistedFactory;
 import dagger.assisted.AssistedInject;
-import de.uniks.stp.Constants;
 import de.uniks.stp.ViewLoader;
 import de.uniks.stp.model.User;
-import de.uniks.stp.router.RouteArgs;
-import de.uniks.stp.router.Router;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 
 import java.io.IOException;
@@ -21,12 +17,10 @@ public class UserListEntry extends HBox {
     @FXML
     private Label userNameLabel;
 
-    private final User user;
-    private final Router router;
+    protected final User user;
 
     @AssistedInject
     public UserListEntry(ViewLoader viewLoader,
-                         Router router,
                          @Assisted final User user) {
         final FXMLLoader fxmlLoader = viewLoader.getFXMLComponentLoader(Components.USER_LIST_ENTRY);
         fxmlLoader.setRoot(this);
@@ -40,16 +34,8 @@ public class UserListEntry extends HBox {
         }
 
         this.user = user;
-        this.router = router;
 
         setUserName(user.getName());
-
-        this.setOnMouseClicked(this::handleClick);
-    }
-
-    private void handleClick(MouseEvent mouseEvent) {
-        RouteArgs args = new RouteArgs().addArgument(Constants.ROUTE_PRIVATE_CHAT_ARGS, user.getId());
-        router.route(Constants.ROUTE_MAIN + Constants.ROUTE_HOME + Constants.ROUTE_PRIVATE_CHAT, args);
     }
 
     public void setUserName(final String userName) {
