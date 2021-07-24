b07aa14b80153e8271d88541d6917c3c8dbe7018
Luca Hertel <lucah199800@gmail.com>
2021-07-24 14:37:38 +0200

    [TEST] settings modal

---
+++ app/src/test/java/de/uniks/stp/controller/SettingsModalTest.java
@@ -0,0 +1,153 @@
+package de.uniks.stp.controller;
+
+import com.sun.prism.shader.DrawPgram_LinearGradient_REFLECT_AlphaTest_Loader;
+import de.uniks.stp.AccordApp;
+import de.uniks.stp.Constants;
+import de.uniks.stp.Editor;
+import de.uniks.stp.ViewLoader;
+import de.uniks.stp.component.KeyBasedComboBox;
+import de.uniks.stp.dagger.components.test.AppTestComponent;
+import de.uniks.stp.dagger.components.test.SessionTestComponent;
+import de.uniks.stp.model.Category;
+import de.uniks.stp.model.Server;
+import de.uniks.stp.model.User;
+import de.uniks.stp.network.rest.SessionRestClient;
+import de.uniks.stp.network.websocket.WSCallback;
+import de.uniks.stp.network.websocket.WebSocketClientFactory;
+import de.uniks.stp.network.websocket.WebSocketService;
+import de.uniks.stp.router.RouteArgs;
+import de.uniks.stp.router.Router;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import kong.unirest.Callback;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.json.JSONObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.util.WaitForAsyncUtils;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@ExtendWith(ApplicationExtension.class)
+public class SettingsModalTest {
+    @Mock
+    private HttpResponse<JsonNode> res;
+
+    @Captor
+    private ArgumentCaptor<Callback<JsonNode>> callbackCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> stringArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<WSCallback> wsCallbackArgumentCaptor;
+
+    private HashMap<String, WSCallback> endpointCallbackHashmap;
+
+    private AccordApp app;
+    private Router router;
+    private Editor editor;
+    private WebSocketClientFactory webSocketClientFactoryMock;
+    private WebSocketService webSocketService;
+    private SessionRestClient restMock;
+
+    @Start
+    public void start(Stage stage) {
+        endpointCallbackHashmap = new HashMap<>();
+        // start application
+        MockitoAnnotations.initMocks(this);
+        app = new AccordApp();
+        app.setTestMode(true);
+        app.start(stage);
+
+        AppTestComponent appTestComponent = (AppTestComponent) app.getAppComponent();
+        router = appTestComponent.getRouter();
+        editor = appTestComponent.getEditor();
+        User currentUser = editor.createCurrentUser("TestUser1", true).setId("1");
+        editor.setCurrentUser(currentUser);
+
+        SessionTestComponent sessionTestComponent = appTestComponent
+            .sessionTestComponentBuilder()
+            .currentUser(currentUser)
+            .userKey("123-45")
+            .build();
+        app.setSessionComponent(sessionTestComponent);
+
+        webSocketClientFactoryMock = sessionTestComponent.getWebSocketClientFactory();
+        webSocketService = sessionTestComponent.getWebsocketService();
+        restMock = sessionTestComponent.getSessionRestClient();
+        webSocketService.init();
+    }
+
+    @Test
+    public void SettingsTest(FxRobot robot) {
+        Platform.runLater(() -> router.route(Constants.ROUTE_MAIN + Constants.ROUTE_HOME + Constants.ROUTE_LIST_ONLINE_USERS, new RouteArgs()));
+        WaitForAsyncUtils.waitForFxEvents();
+
+        robot.clickOn("#settings-gear");
+        robot.clickOn("#settings-cancel-button");
+        robot.clickOn("#settings-gear");
+
+        KeyBasedComboBox languageComboBox = robot.lookup("#combo-select-language").query();
+        Platform.runLater(() -> languageComboBox.setSelection("en"));
+        WaitForAsyncUtils.waitForFxEvents();
+
+        KeyBasedComboBox notificationSoundComboBox = robot.lookup("#combo-select-notification-sound").query();
+        Platform.runLater(() -> notificationSoundComboBox.setSelection("light-button.wav"));
+        WaitForAsyncUtils.waitForFxEvents();
+
+        robot.clickOn("#settings-apply-button");
+
+        robot.clickOn("#settings-gear");
+
+        KeyBasedComboBox languageComboBox2 = robot.lookup("#combo-select-language").query();
+        Assertions.assertEquals("en", languageComboBox2.getSelection());
+        Platform.runLater(() -> languageComboBox2.setSelection("de"));
+        WaitForAsyncUtils.waitForFxEvents();
+
+        KeyBasedComboBox notificationSoundComboBox2 = robot.lookup("#combo-select-notification-sound").query();
+        Assertions.assertEquals("light-button.wav", notificationSoundComboBox2.getSelection());
+        Platform.runLater(() -> notificationSoundComboBox2.setSelection("gaming-lock.wav"));
+        WaitForAsyncUtils.waitForFxEvents();
+
+        robot.clickOn("#settings-apply-button");
+    }
+
+    @AfterEach
+    void tear(){
+        restMock = null;
+        webSocketClientFactoryMock = null;
+        editor = null;
+        webSocketService = null;
+        router = null;
+        res = null;
+        callbackCaptor = null;
+        stringArgumentCaptor = null;
+        wsCallbackArgumentCaptor = null;
+        endpointCallbackHashmap = null;
+        Platform.runLater(app::stop);
+        WaitForAsyncUtils.waitForFxEvents();
+        app = null;
+    }
+}
