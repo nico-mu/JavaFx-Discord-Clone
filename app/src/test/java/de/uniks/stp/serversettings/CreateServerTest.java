b3e2c1957ee10e2ebeb200eb387edade59897ea4
Luca Hertel <lucah199800@gmail.com>
2021-08-04 13:36:36 +0200

    [CHORE] use constants

--- app/src/test/java/de/uniks/stp/serversettings/CreateServerTest.java
+++ app/src/test/java/de/uniks/stp/serversettings/CreateServerTest.java
@@ -5,8 +5,10 @@ import de.uniks.stp.AccordApp;
 import de.uniks.stp.Constants;
 import de.uniks.stp.Editor;
 import de.uniks.stp.ViewLoader;
+import de.uniks.stp.component.NavBarCreateServer;
 import de.uniks.stp.dagger.components.test.AppTestComponent;
 import de.uniks.stp.dagger.components.test.SessionTestComponent;
+import de.uniks.stp.modal.CreateServerModal;
 import de.uniks.stp.model.Category;
 import de.uniks.stp.model.Server;
 import de.uniks.stp.model.User;
@@ -156,40 +158,43 @@ public class CreateServerTest {
 
     @Test
     public void joinServerTest(FxRobot nico) {
+        String testUserName = "TestUser1";
+        String password = "password";
+
         Platform.runLater(() -> router.route(Constants.ROUTE_MAIN + Constants.ROUTE_HOME + Constants.ROUTE_LIST_ONLINE_USERS, new RouteArgs()));
         WaitForAsyncUtils.waitForFxEvents();
 
-        nico.clickOn("#create-server");
-        nico.clickOn("#add-server-join-button");
+        nico.clickOn(NavBarCreateServer.CREATE_SERVER_ID);
+        nico.clickOn(CreateServerModal.ADD_SERVER_JOIN_BUTTON);
 
-        Label errorLabel = nico.lookup("#error-message-label").query();
+        Label errorLabel = nico.lookup(CreateServerModal.ADD_SERVER_ERROR_LABEL).query();
         Assertions.assertNotEquals("", errorLabel.getText());
 
-        nico.clickOn("#server-address-text-field");
+        nico.clickOn(CreateServerModal.ADD_SERVER_TEXT_FIELD_SERVER_ADDRESS);
         String serverId = "sId";
         String inviteId = "iId";
-        nico.write("https://ac.uniks.de/api/servers/" + serverId + "/invites/" + inviteId);
-        nico.clickOn("#add-server-join-button");
+        nico.write(Constants.REST_SERVER_BASE_URL + Constants.SERVERS_PATH + "/" + serverId + Constants.REST_INVITES_PATH + "/" + inviteId);
+        nico.clickOn(CreateServerModal.ADD_SERVER_JOIN_BUTTON);
 
         JSONObject j = new JSONObject()
             .put("status", "failure");
         when(res.getBody()).thenReturn(new JsonNode(j.toString()));
         when(res.isSuccess()).thenReturn(false);
 
-        verify(restMock).joinServer(eq(serverId), eq(inviteId), eq("TestUser1"), eq("password"), callbackCaptor.capture());
+        verify(restMock).joinServer(eq(serverId), eq(inviteId), eq(testUserName), eq(password), callbackCaptor.capture());
         Callback<JsonNode> callback = callbackCaptor.getValue();
         callback.completed(res);
         WaitForAsyncUtils.waitForFxEvents();
 
         Assertions.assertNotEquals("", errorLabel.getText());
 
-        JFXTextField addressTextfield = nico.lookup("#server-address-text-field").query();
+        JFXTextField addressTextfield = nico.lookup(CreateServerModal.ADD_SERVER_TEXT_FIELD_SERVER_ADDRESS).query();
         addressTextfield.clear();
-        nico.clickOn("#server-address-text-field");
+        nico.clickOn(CreateServerModal.ADD_SERVER_TEXT_FIELD_SERVER_ADDRESS);
         String serverId2 = "sId2";
         String inviteId2 = "iId2";
-        nico.write("https://ac.uniks.de/api/servers/" + serverId2 + "/invites/" + inviteId2);
-        nico.clickOn("#add-server-join-button");
+        nico.write(Constants.REST_SERVER_BASE_URL + Constants.SERVERS_PATH + "/" + serverId2 + Constants.REST_INVITES_PATH + "/" + inviteId2);
+        nico.clickOn(CreateServerModal.ADD_SERVER_JOIN_BUTTON);
         Assertions.assertEquals("", errorLabel.getText());
 
         JSONObject j2 = new JSONObject()
@@ -197,7 +202,7 @@ public class CreateServerTest {
         when(res.getBody()).thenReturn(new JsonNode(j2.toString()));
         when(res.isSuccess()).thenReturn(true);
 
-        verify(restMock).joinServer(eq(serverId2), eq(inviteId2), eq("TestUser1"), eq("password"), callbackCaptor.capture());
+        verify(restMock).joinServer(eq(serverId2), eq(inviteId2), eq(testUserName), eq(password), callbackCaptor.capture());
         Callback<JsonNode> callback2 = callbackCaptor.getValue();
         callback2.completed(res);
         WaitForAsyncUtils.waitForFxEvents();
