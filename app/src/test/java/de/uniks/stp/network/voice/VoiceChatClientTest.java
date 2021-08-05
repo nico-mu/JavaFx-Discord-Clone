4e8b766967ebdf89719731d351affa18917c39a5
Marco Wennesz <marco.wennesz@student.uni-kassel.de>
2021-08-05 23:01:50 +0200

    [TEST] Create missing tests for VoiceChatClient

--- app/src/test/java/de/uniks/stp/network/voice/VoiceChatClientTest.java
+++ app/src/test/java/de/uniks/stp/network/voice/VoiceChatClientTest.java
@@ -1,12 +1,7 @@
 package de.uniks.stp.network.voice;
 
-import de.uniks.stp.AccordApp;
-import de.uniks.stp.dagger.components.test.AppTestComponent;
-import de.uniks.stp.dagger.components.test.DaggerAppTestComponent;
-import de.uniks.stp.dagger.components.test.SessionTestComponent;
 import de.uniks.stp.model.Channel;
 import de.uniks.stp.model.User;
-import javafx.stage.Stage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,43 +9,39 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import javax.sound.sampled.Mixer;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 public class VoiceChatClientTest {
     @Mock
-    private Mixer speaker1;
+    private VoiceChatService voiceChatService;
     @Mock
-    private Mixer speaker2;
+    private DatagramSocket datagramSocket;
     @Mock
-    private Mixer microphone1;
-    @Mock
-    private Mixer microphone2;
+    private InetAddress address;
     private VoiceChatClient voiceChatClient;
+    private Channel channel;
+    private User currentUser;
+    private User otherUser;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        AppTestComponent appTestComponent = DaggerAppTestComponent
-            .builder()
-            .application(Mockito.mock(AccordApp.class))
-            .primaryStage(Mockito.mock(Stage.class))
-            .build();
 
-        User currentUser = new User().setName("test-user1").setId("test-user1-id");
+        currentUser = new User().setName("test-user1").setId("test-user1-id");
+        otherUser = new User().setId("abc").setName("user1");
 
-        SessionTestComponent sessionTestComponent = appTestComponent
-            .sessionTestComponentBuilder()
-            .currentUser(currentUser)
-            .userKey("123-45")
-            .build();
+        channel = new Channel().setId("channelId").withAudioMembers(otherUser);
 
-        voiceChatClient = sessionTestComponent.getVoiceChatClientFactory().create(null, new Channel(), speaker1, microphone1);
-    }
-
-    @Test
-    public void changeDevicesTest() {
-        voiceChatClient.changeSpeaker(speaker2);
-        voiceChatClient.changeMicrophone(microphone2);
+        voiceChatClient = new VoiceChatClient(voiceChatService, channel, currentUser)
+            .setDatagramSocket(datagramSocket)
+            .setAddress(address);
     }
 
     @Test
@@ -60,12 +51,90 @@ public class VoiceChatClientTest {
         voiceChatClient.withoutFilteredUsers(otherUser);
     }
 
+    @Test
+    public void userUnMuteUnAudioTest() {
+        SourceDataLine sourceDataLineMock = Mockito.mock(SourceDataLine.class);
+        TargetDataLine targetDataLineMock = Mockito.mock(TargetDataLine.class);
+        try {
+            doReturn(sourceDataLineMock).when(voiceChatService).createUsableSourceDataLine();
+            doReturn(targetDataLineMock).when(voiceChatService).createUsableTargetDataLine();
+        } catch (LineUnavailableException ignored) { }
+        doReturn(true).when(voiceChatService).isMicrophoneAvailable();
+        doReturn(true).when(voiceChatService).isSpeakerAvailable();
+        currentUser.setMute(true);
+        currentUser.setAudioOff(true);
+        otherUser.setMute(true);
+
+        voiceChatClient.init();
+
+        currentUser.setMute(false);
+        currentUser.setAudioOff(false);
+        otherUser.setMute(false);
+
+        currentUser.setMute(true);
+        currentUser.setAudioOff(true);
+        otherUser.setMute(true);
+        try {
+            Thread.sleep(3*1000); // receiveAndPlay & recordAndSend executables need some time
+        } catch (InterruptedException ignored) { }
+    }
+
+    @Test
+    public void otherUserTest() {
+        SourceDataLine sourceDataLineMock = Mockito.mock(SourceDataLine.class);
+        try {
+            doReturn(sourceDataLineMock).when(voiceChatService).createUsableSourceDataLine();
+        } catch (LineUnavailableException ignored) { }
+        doReturn(true).when(voiceChatService).isSpeakerAvailable();
+        voiceChatClient.init();
+
+        channel.withoutAudioMembers(otherUser);
+        channel.withAudioMembers(otherUser);
+    }
+
+    @Test
+    public void noLinesAvailableTest() {
+        try {
+            doThrow(LineUnavailableException.class).when(voiceChatService).createUsableSourceDataLine();
+            doThrow(LineUnavailableException.class).when(voiceChatService).createUsableTargetDataLine();
+        } catch (LineUnavailableException ignored) { }
+        doReturn(true).when(voiceChatService).isMicrophoneAvailable();
+        doReturn(true).when(voiceChatService).isSpeakerAvailable();
+        voiceChatClient.init();
+
+        final User user1 = new User().setId("abc").setName("user1");
+        channel.withAudioMembers(user1);
+        channel.withoutAudioMembers(user1);
+
+        voiceChatClient.onMicrophoneChanged();
+        voiceChatClient.onSpeakerChanged();
+    }
+
+    @Test
+    public void devicesChangeTest() {
+        SourceDataLine sourceDataLineMock = Mockito.mock(SourceDataLine.class);
+        TargetDataLine targetDataLineMock = Mockito.mock(TargetDataLine.class);
+        try {
+            doReturn(sourceDataLineMock).when(voiceChatService).createUsableSourceDataLine();
+            doReturn(targetDataLineMock).when(voiceChatService).createUsableTargetDataLine();
+        } catch (LineUnavailableException ignored) { }
+        doReturn(true).when(voiceChatService).isMicrophoneAvailable();
+        doReturn(true).when(voiceChatService).isSpeakerAvailable();
+
+        voiceChatClient.init();
+        voiceChatClient.onMicrophoneChanged();
+        voiceChatClient.onSpeakerChanged();
+
+    }
+
     @AfterEach
     public void tearDown() {
-        speaker1 = null;
-        speaker2 = null;
-        microphone1 = null;
-        microphone2 = null;
         voiceChatClient.stop();
+        voiceChatService = null;
+        datagramSocket = null;
+        address = null;
+        voiceChatClient = null;
+        channel = null;
+        currentUser = null;
     }
 }
